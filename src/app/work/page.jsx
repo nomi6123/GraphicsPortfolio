55c6bad4551c05bb26fdad8d1ca2af68a56565c4
nomi6123 <codewithnoman6123@gmail.com>
2025-10-27 14:51:33 +0500

    work page updated

--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -2,17 +2,66 @@ export default function WorkPage() {
   const works = [
     {
       title: "Modern Logo Design",
-      img: "/works/logo1.jpg",
+      img: "/1.jpg",
       desc: "A sleek minimal logo design using geometric balance.",
     },
     {
       title: "Business Card Layout",
-      img: "/works/card.jpg",
+      img: "/2.jpeg",
       desc: "Professional card design with a clean typography structure.",
     },
     {
       title: "Brand Poster",
-      img: "/works/poster.jpg",
+      img: "/3.jpeg",
+      desc: "Creative marketing poster for a brand launch event.",
+    },    {
+      title: "Modern Logo Design",
+      img: "/4.jpg",
+      desc: "A sleek minimal logo design using geometric balance.",
+    },
+    {
+      title: "Business Card Layout",
+      img: "/5.jpeg",
+      desc: "Professional card design with a clean typography structure.",
+    },
+    {
+      title: "Brand Poster",
+      img: "/6.jpeg",
+      desc: "Creative marketing poster for a brand launch event.",
+    },
+        {
+      title: "Modern Logo Design",
+      img: "/7.jpeg",
+      desc: "A sleek minimal logo design using geometric balance.",
+    },
+    {
+      title: "Business Card Layout",
+      img: "/8.jpeg",
+      desc: "Professional card design with a clean typography structure.",
+    },
+    {
+      title: "Brand Poster",
+      img: "/9.jpeg",
+      desc: "Creative marketing poster for a brand launch event.",
+    },
+        {
+      title: "Modern Logo Design",
+      img: "/10.jpeg",
+      desc: "A sleek minimal logo design using geometric balance.",
+    },
+    {
+      title: "Business Card Layout",
+      img: "/11.jpeg",
+      desc: "Professional card design with a clean typography structure.",
+    },
+    {
+      title: "Brand Poster",
+      img: "/12.jpeg",
+      desc: "Creative marketing poster for a brand launch event.",
+    },
+ {
+      title: "Brand Poster",
+      img: "/13.jpeg",
       desc: "Creative marketing poster for a brand launch event.",
     },
   ];
